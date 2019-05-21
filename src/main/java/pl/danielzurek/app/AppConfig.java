fc65879079e1a43ec3ca6a5503c0a50860330f33
danielzurek <danielzurek1@gmail.com>
2019-05-21 18:12:37 +0200

    potential security vulnerability has been fixed

--- src/main/java/pl/danielzurek/app/AppConfig.java
+++ src/main/java/pl/danielzurek/app/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
@@ -15,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import pl.danielzurek.converter.*;
-import pl.danielzurek.entity.Incident;
 import pl.danielzurek.service.CustomerService;
 import pl.danielzurek.service.CustomerServiceImpl;
 
@@ -26,7 +24,7 @@ import javax.persistence.EntityManagerFactory;
 @ComponentScan(basePackages = {"pl.danielzurek"})
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "pl.danielzurek.repository")
-@ImportResource({"classpath:META-INF/appconfig-security.xml",  "classpath:META-INF/appconfig-mvc.xml"})
+@ImportResource({"classpath:META-INF/appconfig-security.xml", "classpath:META-INF/appconfig-messages.xml"})
 
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
